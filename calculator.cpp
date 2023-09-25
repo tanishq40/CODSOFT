b87b62921cf89ae3947918197aed69e07f7f180c
TANISHQ SHUKLA <139757865+tanishq40@users.noreply.github.com>
2023-09-25 22:15:49 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a ";
+    cin>>a;
+    cout<<"Enter the value of b";
+    cin>>b;
+    char op;
+    cout<<"Enter the operations you want to perform"<<endl;
+    cin>>op;
+    switch(op){
+        case'+':cout<<(a+b)<<endl;
+        break;
+        case'-':cout<<(a-b)<<endl;
+        break;
+        case'*':cout<<(a*b)<<endl;
+        break;
+        case'/':cout<<(a/b)<<endl;
+        break;
+        default:cout<<"please enter a valid opeartions"<<endl;
+        }
+}
